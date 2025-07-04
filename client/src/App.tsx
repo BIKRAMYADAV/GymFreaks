dc631cac71576288b57394e442fe0f127abd44de
BIKRAMYADAV <bikram.deep.yadav.11@gmail.com>
2025-07-04 23:58:49 +0530

    feat: designed the landing page of phase-1

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,15 @@
-
+import Home from "./pages/Home"
+import { Route, Routes } from "react-router-dom"
 function App() {
  
 
   return (
-    <>
-    
-    </>
+    <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/progress" element={<h1>bikram</h1>}/>
+    <Route path="/gyms" element={<h1>bikram</h1>}/>
+    <Route path="/profile" element={<h1>bikram</h1>}/>
+    </Routes>
   )
 }
 
---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,41 @@
+import { Link } from "react-router-dom"
+
+function Home() {
+  return (
+
+<div
+  className="relative h-screen bg-cover bg-center"
+  style={{ backgroundImage: "url('/bg.jpg')" }}
+>
+  <div className="absolute inset-0 bg-gradient-to-r from-black/60 via-transparent to-black/60"></div>
+
+  <div className="relative z-10 text-white p-10">
+     <h1 className="text-9xl font-extrabold tracking-wide text-transparent bg-clip-text bg-gradient-to-r
+  from-red-900 via-red-800 to-red-400 drop-shadow-lg opacity-100">
+   GymFreaks
+</h1>
+    <p className="mt-4 text-lg opacity-50">Track your fitness, protein, and progress.</p>
+  </div>
+    <div className=" absolute bottom-10 left-1/2 transform -translate-x-1/2 flex flex-col sm:flex-row gap-4 z-10">
+        <Link to="/progress">
+          <button className="cursor-pointer px-6 py-3 rounded-2xl bg-gradient-to-r  text-white text-lg font-semibold shadow-md hover:scale-105 transition">
+            Daily Progress
+          </button>
+        </Link>
+        <Link to="/gyms">
+          <button className="cursor-pointer px-6 py-3 rounded-2xl bg-gradient-to-r  text-white text-lg font-semibold shadow-md hover:scale-105 transition">
+            Find Gyms
+          </button>
+        </Link>
+        <Link to="/profile">
+          <button className="cursor-pointer px-6 py-3 rounded-2xl bg-gradient-to-r  text-white text-lg font-semibold shadow-md hover:scale-105 transition">
+            Profile
+          </button>
+        </Link>
+      </div>
+</div>
+
+  )
+}
+
+export default Home
